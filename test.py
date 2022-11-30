f7d144fa26f86613fbb09bd4393b529de39f4dc6
Fabio Lolicato <lolicato.fabio@gmail.com>
2022-12-01 00:04:50 +0100

    Update test.py

--- test.py
+++ test.py
@@ -4,3 +4,11 @@ import numpy as np
 
 age = st.slider('How old are you?', 0, 130, 25)
 st.write("I'm ", age, 'years old')
+
+
+options = st.multiselect(
+    'What are your favorite colors',
+    ['Green', 'Yellow', 'Red', 'Blue'],
+    ['Yellow', 'Red'])
+
+st.write('You selected:', options)
