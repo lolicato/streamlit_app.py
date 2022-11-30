c06ce9b6ce3be1808f63b88c3b9886bb109b0769
Fabio Lolicato <lolicato.fabio@gmail.com>
2022-12-01 00:06:34 +0100

    Update test.py

--- test.py
+++ test.py
@@ -12,3 +12,16 @@ options = st.multiselect(
     ['Yellow', 'Red'])
 
 st.write('You selected:', options)
+
+@st.cache(allow_output_mutation=True)
+def get_data():
+    return []
+
+user_id = st.text_input("User ID")
+foo = st.slider("foo", 0, 100)
+bar = st.slider("bar", 0, 100)
+
+if st.button("Add row"):
+    get_data().append({"UserID": user_id, "foo": foo, "bar": bar})
+
+st.write(pd.DataFrame(get_data()))
